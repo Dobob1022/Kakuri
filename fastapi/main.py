a9ff349d69f7e61eb07e5d04324d28db4a6cc12f
Dobob1022 <dobob@dobob.kr>
2022-09-23 00:14:46 +0900

    Andrew Help the Frontend

--- fastapi/main.py
+++ fastapi/main.py
@@ -34,9 +34,9 @@ testjob = [{
         "manager":"dobob"
     },
     {
-        "JobId":"20220922_R_KABANK",
-        "status":"active",
-        "Name":"카뱅-상암-RMA작업",
+        "JobId":"20220922_N_TOSS",
+        "status":"inactive",
+        "Name":"토스모니터20대납품",
         "target":"sp-dat-01",
         "Description":"TESTJOB",
         "Progress":"100",
@@ -56,6 +56,30 @@ def root():
 def LoadJobs():
     return json.dumps(testjob)
 
+# @app.get("/api/job/{job_id}")
+# def read_jobid(job_id):
+#     return {"jobid":job_id}
+
+@app.get("/api/job/20220922_R_KABANK")
+def read_jobid():
+    result = {
+        "JobId":"20220922_R_KABANK",
+        "status":"active",
+        "Name":"카뱅-상암-RMA작업",
+        "target":"sp-dat-01",
+        "Description":"TESTJOB",
+        "Progress":"40",
+        "AddDate":"2022-09-22",
+        "DeadLine":"2022-09-23",
+        "LastEdit":"Dobob",
+        "LastEditTime":"2022-09-22",
+        "manager":"dobob"
+    }
+    return json.dumps(result)
+
+
+
 @app.post("/api/job")
 def InsertJobs():
     return {"msg":"HAHA"}
+
