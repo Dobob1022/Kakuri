05e014edece18743547c472dde69843e382abaee
Dobob1022 <dobob@dobob.kr>
2022-09-23 12:28:09 +0900

    DB Insert Function is NOT Working

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from datetime import datetime
+from fastapi import FastAPI,Request
 from fastapi.middleware.cors import CORSMiddleware
 from modules import db
+from pydantic import BaseModel
+import datetime as dt
 
 #for TEST
 
@@ -19,6 +22,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#JobClass
+
+# class Job(BaseModel):
+#     JobId : str
+#     stataus : str
+#     Name : str
+#     target : str
+#     Description : str
+#     Progress : str
+#     AddDate : str
+#     DeadLine : str
+#     LastEdit : str
+#     LastEditTime : str
+#     manager : str
+#     customerManger : str
+#     customerManager_contect : str
+
+class InsertJob(BaseModel):
+    JobType : str
+    EndUser : str
+    JobName : str
+    Location : str
+    Target : str
+    Description : str
+    DeadLine : str
+    manager : str
+    customerManger : str
+    customerManager_contect : str
+
 
 testjob = [{
         "JobId":"20220922_R_KABANK",
@@ -31,7 +63,9 @@ testjob = [{
         "DeadLine":"2022-09-23",
         "LastEdit":"Dobob",
         "LastEditTime":"2022-09-22",
-        "manager":"dobob"
+        "manager":"dobob",
+        "customerManger":"조성본",
+        "customerManager_contect":"010-1111-1111"
     },
     {
         "JobId":"20220922_N_TOSS",
@@ -73,13 +107,30 @@ def read_jobid():
         "DeadLine":"2022-09-23",
         "LastEdit":"Dobob",
         "LastEditTime":"2022-09-22",
-        "manager":"dobob"
+        "manager":"dobob",
+        "customerManger":"조성본",
+        "customerManager_contect":"010-1111-1111"
     }
     return json.dumps(result)
 
 
-
 @app.post("/api/job")
-def InsertJobs():
-    return {"msg":"HAHA"}
+def get_body(request:InsertJob):
+    print(request)
+    status = "Active"
+    #job id get
+    date = dt.datetime.now().date()
+    enduser = request.EndUser
+    if request.JobType == "New":
+        JobID = str(date)+"-"+"N"+"-"+enduser
+        db.insert_job(JobID=JobID,JobType=request.JobType,Status=status,Name=request.JobName,EndUser=request.EndUser,Location=request.Location,Target=request.Target)
+    elif request.JobType == "Delivery":
+        JobID = str(date)+"-"+"D"+"-"+enduser
+    elif request.JobType == "RMA":
+        JobID = str(date)+"-"+"R"+"-"+enduser
+    else:
+        return "fuq"
 
+
+
+    return request
--- fastapi/modules/db.py
+++ fastapi/modules/db.py
@@ -11,15 +11,41 @@ Base = declarative_base()
 class Job(Base):
     __tablename__ = 'joblist'
     job_id = Column(Integer, primary_key=True, unique=True)
+    status = Column(String)
     name = Column(String)
+    target = Column(String)
     description = Column(String)
     progress = Column(String)
     adddate = Column(DateTime, default=datetime.datetime.utcnow)
     deadline = Column(DateTime)
-    lastedit_time = Column(DateTime)
     lastedit_user = Column(String)
+    lastedit_time = Column(DateTime)
     manager = Column(String)
+    customerManager = Column(String)
+    customerManager_contect = Column(String)
 
+class InsertJob(Base):
+    __tablename__ = 'insertjob'
+    # Input of Json
+    job_id=Column(String,primary_key=True)
+    job_type = Column(String)
+    job_name = Column(String)
+    EndUser = Column(String)
+    location = Column(String)
+    target = Column(String)
+    description = Column(String)
+    deadLine = Column(String)
+    manager = Column(String)
+    customer_manger = Column(String)
+    customerManager_contect = Column(String)
+    #Db Input
+    progress = Column(Integer,default=0)
+    add_date = Column(DateTime, default=datetime.datetime.utcnow)
+    lastedit_user = Column(String)
+    lastedit_time = Column(DateTime)
+    status = Column(String)
+    
+    
     
 class Auth(Base):
     __tablename__ ="auth"
@@ -32,6 +58,12 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine,autocommit=False,autoflush=False)
 session = Session()
 
+#Insert Job
+def insert_job(JobID,JobType,Status,Name,EndUser,Location,Target):
+    query = InsertJob(job_id=JobID,job_type=JobType,status=Status,job_name=Name,EndUser=EndUser,location=Location,target=Target)
+    session.add(query)
+    session.commit()
+    session.close()
 ##Initialize##
 
 
